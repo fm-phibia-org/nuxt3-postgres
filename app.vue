a44e66c8b5e7e4d78198adfab673a301e703e84f
fumiyamatuura <f.tbh.depapls238@gmail.com>
2023-06-17 16:42:25 +0900

    インデント調整

--- app.vue
+++ app.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
-import { useCounterStore } from "@/store/counter"
-import { storeToRefs } from "pinia"
-const counterStore = useCounterStore()
-const { increment } = counterStore
-const { count } = storeToRefs(counterStore)
+  import { useCounterStore } from "@/store/counter"
+  import { storeToRefs } from "pinia"
+  const counterStore = useCounterStore()
+  const { increment } = counterStore
+  const { count } = storeToRefs(counterStore)
 </script>
 
 <template>
   <div>
-    <h1>Count:{{ count }}</h1>
-    <button @click="increment">たすよ</button>
+    <h1>
+      Count:{{ count }}
+    </h1>
+    <button 
+      @click="increment"
+    >
+      たすよ
+    </button>
     <br />
-    <nuxt-link to="/">もどる</nuxt-link>
+    <nuxt-link
+      to="/"
+    >
+      もどる
+    </nuxt-link>
   </div>
   <div>
-    <v-btn>aaaa</v-btn>
-    <div class="
+    <v-btn>
+      aaaa
+    </v-btn>
+    <div
+      class="
         text-3xl
         font-bold
         underline
         bg-gray-700
-      ">
+      "
+    >
       トップ
     </div>
   </div>
